774e4913d19970e3bc374dd9c49533833a043a8f
Dmitry Pchellovod <pchellovod@gmail.com>
2016-08-08 15:39:37 +0000

    add product unique test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,22 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+  
+  test "product is not valid without a unique title" do
+    # если у товара нет уникального названия, то он недопустим
+    product = Product.new(title: products(:ruby).title,
+      description: "yyy",
+      price:      1,
+      image_url: "fred.gif")
+      
+      assert product.invalid?
+      
+      assert_equal ["has already been taken"], product.errors[:title]
+        #уже было использовано
+  end
+  
+  
   test "product attributes must not be empty" do
     # свойства товара не должны оставаться пустыми
     product = Product.new
