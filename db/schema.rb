a05352f22d6430f425f912c32f40ac2cb8c0a9ba
Dmitry Pchellovod <pchellovod@gmail.com>
2016-08-22 11:25:11 +0000

    add cart migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822110709) do
+ActiveRecord::Schema.define(version: 20160822111341) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
