6079b357cec2661dbc79e78f764b2803eb9731d7
Dmitry Pchellovod <pchellovod@gmail.com>
2016-09-04 17:49:28 +0000

    add received method to order_notifier

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,14 +1,13 @@
 class OrderNotifier < ApplicationMailer
-  default from: 'Dmitry Pchellovod <pchellovod@gmail.com>'
+  default from: 'Dmitry <depot@example.com>'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_notifier.received.subject
   #
-  def received
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def received(order)
+    @order = order
+    mail to: order.email, subject: 'Подтверждение заказа в Pragmatic Store'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
