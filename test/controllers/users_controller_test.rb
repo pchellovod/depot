2879f6a41839553ba8df6601a12e85b317cbcda8
Dmitry Pchellovod <pchellovod@gmail.com>
2016-09-26 11:39:19 +0000

    refresh create test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -18,10 +18,11 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { name: @user.name, password: 'secret', password_confirmation: 'secret' }
-    end
+      post :create, user: { name: 'sam', password: 'secret',
+            password_confirmation: 'secret' }
+end
 
-    assert_redirected_to user_path(assigns(:user))
+    assert_redirected_to users_path
   end
 
   test "should show user" do
