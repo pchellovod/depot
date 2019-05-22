13010d4de8332a9a8003720256232d88cb0aa766
Dmitry <dvoryankindm@gmail.com>
2019-05-22 21:09:13 +0500

    bundle updated and fix spaces

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,32 +1,31 @@
 class Product < ActiveRecord::Base
-    has_many :line_items
-    has_many :orders, through: :line_items
-    
-    before_destroy :ensure_not_referenced_by_any_line_item
-    
-    validates :title, :description, :image_url, presence: true
-    validates :price, numericality: {greater_than_or_equal_to: 0.01}
-    validates :title, uniqueness: true
-    validates :image_url, allow_blank: true, format: {
-        with: %r{\.(gif|jpg|png)\Z}i,
-        message: 'must be a URL for GIF, JPG or PNG image.'
-        # URL должен указывать на изображение формата GIF, JPG или PNG
-}
+  has_many :line_items
+  has_many :orders, through: :line_items
 
-    def self.latest
-        Product.order(:updated_at).last
-    end
-    
-    private
-    
-    # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
-    def ensure_not_referenced_by_any_line_item
-        if line_items.empty?
-            return true
-        else
-            errors.add(:base, 'существуют товарные позиции')
-            return false
-        end
-    end
+  before_destroy :ensure_not_referenced_by_any_line_item
 
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+    # URL должен указывать на изображение формата GIF, JPG или PNG
+  }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+
+  private
+
+  # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'существуют товарные позиции')
+      return false
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
 class User < ActiveRecord::Base
-    validates :name, presence: true, uniqueness: true
-    has_secure_password
-    
-    after_destroy :ensure_an_admin_remains
-    
-    private
-        def ensure_an_admin_remains
-            if User.count.zero?
-                raise "You can not delete last user"
-            end
-        end
-        
-    
+  validates :name, presence: true, uniqueness: true
+  has_secure_password
+
+  after_destroy :ensure_an_admin_remains
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+         raise "You can not delete last user"
+      end
+    end
 end
